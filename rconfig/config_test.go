3f8997429f3ca3bb13564616b1a85c259d4ba8dd
by <temporarycrash@gmail.com>
2021-02-26 18:50:49 +0800

    add testing

--- rconfig/config_test.go
+++ rconfig/config_test.go
@@ -17,66 +17,86 @@ func TestRegConfig(t *testing.T) {
 
 func TestGetConfig(t *testing.T) {
 	Reg("default", "tests/config.yml")
-
 	s := Get("default")
 
-	type Mysql struct {
-		DB   string
-		Host string
-		Port int
-	}
-	var mysql Mysql
-	s.ValTo("mysql", &mysql)
-	assert.Equal(t, Mysql{
-		DB:   "default",
-		Host: "127.0.0.1",
-		Port: 3306,
-	}, mysql)
+	var m mysql
+	s.ValTo("mysql", &m)
+	assert.Equal(t, expectedMysql, m)
 
-	type Calendar struct {
-		Days   []int
-		Months map[int]string
-	}
-	var calendar Calendar
-	s.ValTo("level1.calendar", &calendar)
-	assert.Equal(t, Calendar{
-		Days: []int{1, 2, 3, 4, 5},
-		Months: map[int]string{
-			1: "January",
-			2: "February",
-			3: "March",
-		},
-	}, calendar)
+	var c calendar
+	s.ValTo("level1.calendar", &c)
+	assert.Equal(t, expectedCalendar, c)
+
+	var h human
+	s.ValTo("human", &h)
+	assert.Equal(t, expectedHuman, h)
 }
 
 func TestDefConf(t *testing.T) {
 	Reg("default", "tests/config.yml")
 
-	type Mysql struct {
-		DB   string
-		Host string
-		Port int
-	}
-	var mysql Mysql
-	DefConf().ValTo("mysql", &mysql)
-	assert.Equal(t, Mysql{
-		DB:   "default",
-		Host: "127.0.0.1",
-		Port: 3306,
-	}, mysql)
+	var m mysql
+	DefConf().ValTo("mysql", &m)
+	assert.Equal(t, expectedMysql, m)
 
-	type Calendar struct {
-		Days   []int
-		Months map[int]string
-	}
-	var calendar Calendar
-	DefConf().ValTo("level1.calendar", &calendar)
-	assert.Equal(t, Calendar{
-		Days: []int{1, 2, 3, 4, 5},
-		Months: map[int]string{
-			1: "January",
-			2: "February",
-			3: "March",
+	var c calendar
+	DefConf().ValTo("level1.calendar", &c)
+	assert.Equal(t, expectedCalendar, c)
+
+	var h human
+	DefConf().ValTo("human", &h)
+	assert.Equal(t, expectedHuman, h)
+}
+
+type mysql struct {
+	DB   string
+	Host string
+	Port int
+}
+
+type calendar struct {
+	Days   []int
+	Months map[int]string
+}
+
+type fruit struct {
+	ID   int
+	Name string
+}
+type human struct {
+	Name  string
+	Likes []fruit
+}
+
+var expectedMysql = mysql{
+	DB:   "default",
+	Host: "127.0.0.1",
+	Port: 3306,
+}
+
+var expectedCalendar = calendar{
+	Days: []int{1, 2, 3, 4, 5},
+	Months: map[int]string{
+		1: "January",
+		2: "February",
+		3: "March",
+	},
+}
+
+var expectedHuman = human{
+	Name: "mary",
+	Likes: []fruit{
+		{
+			ID:   1,
+			Name: "mango",
 		},
-	}, calendar)
+		{
+			ID:   2,
+			Name: "apple",
+		},
+		{
+			ID:   3,
+			Name: "banana",
+		},
+	},
 }
