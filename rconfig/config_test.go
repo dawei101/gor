d77d826bc7b830a83bc99cebe4c935193a52c31a
by <temporarycrash@gmail.com>
2021-02-26 17:45:33 +0800

    add `DefConf` testing

--- rconfig/config_test.go
+++ rconfig/config_test.go
@@ -48,3 +48,35 @@ func TestGetConfig(t *testing.T) {
 		},
 	}, calendar)
 }
+
+func TestDefConf(t *testing.T) {
+	Reg("default", "tests/config.yml")
+
+	type Mysql struct {
+		DB   string
+		Host string
+		Port int
+	}
+	var mysql Mysql
+	DefConf().ValTo("mysql", &mysql)
+	assert.Equal(t, Mysql{
+		DB:   "default",
+		Host: "127.0.0.1",
+		Port: 3306,
+	}, mysql)
+
+	type Calendar struct {
+		Days   []int
+		Months map[int]string
+	}
+	var calendar Calendar
+	DefConf().ValTo("level1.calendar", &calendar)
+	assert.Equal(t, Calendar{
+		Days: []int{1, 2, 3, 4, 5},
+		Months: map[int]string{
+			1: "January",
+			2: "February",
+			3: "March",
+		},
+	}, calendar)
+}
