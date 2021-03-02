ef6b88c47c94c9c9eeee878882737da86ee97243
by <temporarycrash@gmail.com>
2021-03-02 18:27:04 +0800

    add rkafka

--- rkafka/consumer.go
+++ rkafka/consumer.go
@@ -4,56 +4,64 @@ import (
 	"context"
 	"fmt"
 	"github.com/confluentinc/confluent-kafka-go/kafka"
+	"github.com/rs/xid"
 	"os"
 )
 
+type OnProcess func(msg *kafka.Message, ctx context.Context)
+
+type OnError func(err error, ctx context.Context)
+
 type Consumer struct {
-	clientId  string
+	server   string
+	clientID string
+	groupID  string
+	topic    string
+
 	ctx       context.Context
 	ctxCancel context.CancelFunc
-	topic     string
-	limiter   Limiter
-	*kafka.Consumer
 
-	OnProcess OnProcess
-	OnError   OnError
-	OnLimiter OnLimiter
+	limiter Limiter
+
+	consumer  *kafka.Consumer
+	configMap kafka.ConfigMap
+
+	onProcess OnProcess
+	onError   OnError
+}
+
+type ConsumerConf struct {
+	ClientID string `yaml:"clientID"`
+	GroupID  string `yaml:"groupID"`
+	Server   string `yaml:"server"`
+	Topic    string `yaml:"topic"`
+	Limit    int    `yaml:"limit"`
 }
 
 func (c *Consumer) Run() {
-	err := c.Subscribe(c.topic, nil)
-	if err != nil {
-		fmt.Printf("Consumer: %v create fail. \n", c)
+	if c.onProcess == nil {
 		return
 	}
-	c.Listen()
-
-	fmt.Printf("Consumer: %v create success. \n", c)
-}
-
-func (c *Consumer) Stop() {
-	c.ctxCancel()
-}
-
-func (c *Consumer) Listen() {
-	c.limiter.Watch(c.OnLimiter)
+	err := c.consumer.Subscribe(c.topic, nil)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%% Error: %v \n", err)
+		return
+	}
 
 	for {
 		select {
 		case <-c.ctx.Done():
-			c.ctxCancel()
-			fmt.Printf("consumer %s already stop. \n", c.String())
+			fmt.Printf("consumer `%s` already stop \n", c.consumer.String())
 			return
 		default:
-			ev := c.Poll(100)
+			ev := c.consumer.Poll(100)
 			if ev == nil {
 				continue
 			}
 			switch e := ev.(type) {
 			case *kafka.Message:
 				c.limiter.Run(func() {
-					c.OnProcess(e, c.ctx)
-
+					c.onProcess(e, c.ctx)
 				})
 			case kafka.Error:
 				fmt.Fprintf(os.Stderr, "%% Error: %v: %v\n", e.Code(), e)
@@ -62,4 +70,33 @@ func (c *Consumer) Listen() {
 			}
 		}
 	}
+}
+
+func (c *Consumer) wrap() {
+	if c.limiter.n <= 0 {
+		c.limiter.n = 4
+	}
+
+	c.configMap = kafka.ConfigMap{
+		"client.id": func() kafka.ConfigValue {
+			if c.clientID == "" {
+				c.clientID = xid.New().String()
+			}
+			return c.clientID
+		}(),
+		"bootstrap.servers": func() kafka.ConfigValue {
+			if c.server == "" {
+				c.server = "127.0.0.1"
+			}
+			return c.server
+		}(),
+		"session.timeout.ms": 6000,
+		"group.id":           c.groupID,
+	}
+
+	consumerRaw, err := kafka.NewConsumer(&c.configMap)
+	if err != nil {
+		panic(err)
+	}
+	c.consumer = consumerRaw
 }
--- rkafka/manager.go
+++ rkafka/manager.go
@@ -2,95 +2,64 @@ package rkafka
 
 import (
 	"context"
-	"github.com/confluentinc/confluent-kafka-go/kafka"
-	"roo.bo/rlib"
 	"sync"
 )
 
 type Manager struct {
-	Consumers Consumers
+	consumers map[string]*Consumer
 	ctx       context.Context
 	ctxCancel context.CancelFunc
 	runOnce   sync.Once
-	configs   map[string]*ConsumerConfig
 }
 
-func New() *Manager {
-	m := &Manager{
-		Consumers: Consumers{
-			regs: map[string]*Consumer{},
-		},
+type ManagerConf struct {
+	Consumers map[string]ConsumerConf
+}
+
+func New(managerConf *ManagerConf) *Manager {
+	var m = &Manager{
+		consumers: map[string]*Consumer{},
 	}
-
-	var configs interface{}
-	rlib.LoadDefaultConfig("./config.yml")
-	rlib.DefaultConfig().Value("kafka", &configs, nil)
-
-	topics, ok := configs.(map[string]map[string]string)
-
-	if ok {
-		for topic, config := range topics {
-			conf := m.NewConfig(config["groupId"], topic)
-			m.configs[topic] = conf
-		}
-	}
-
 	m.ctx, m.ctxCancel = context.WithCancel(context.Background())
+
+	for name, consumerConf := range managerConf.Consumers {
+		m.consumers[name] = m.newConsumer(consumerConf)
+	}
+
 	return m
 }
 
-func (m *Manager) OnProcess(topic string, f OnProcess) {
-	if m, ok := m.configs[topic]; ok {
-		m.OnProcess = f
+func (m *Manager) OnProcesses(name string, fn OnProcess) {
+	consumer, ok := m.consumers[name]
+	if ok {
+		consumer.onProcess = fn
 	}
 }
 
-func (m *Manager) NewConfig(groupId kafka.ConfigValue, topic string) *ConsumerConfig {
-	return &ConsumerConfig{
-		ConfigMap: kafka.ConfigMap{
-			"group.id": groupId,
-		},
-
-		Topic: topic,
-		Limit: 10,
-	}
-}
-
-func (m *Manager) NewConsumer(conf *ConsumerConfig) (err error) {
-	conf.Defaults()
-	conf.Wrap()
-	rawC, err := kafka.NewConsumer(&conf.ConfigMap)
-	if err != nil {
-		return
-	}
+func (m *Manager) newConsumer(conf ConsumerConf) *Consumer {
 	var c = &Consumer{
-		clientId: conf.ClientId,
+		clientID: conf.ClientID,
+		groupID:  conf.GroupID,
 		topic:    conf.Topic,
+		server:   conf.Server,
 		limiter: Limiter{
 			n: conf.Limit,
 			c: make(chan struct{}, conf.Limit),
 		},
-
-		Consumer: rawC,
-
-		OnProcess: conf.OnProcess,
-		OnError:   conf.OnError,
-		OnLimiter: conf.OnLimiter,
 	}
+	c.wrap()
 	c.ctx, c.ctxCancel = context.WithCancel(m.ctx)
-	m.Consumers.Set(c)
-	return
+	return c
 }
 
 func (m *Manager) Run() {
 	m.runOnce.Do(func() {
-		for _, conf := range m.configs {
-			if conf.OnProcess != nil {
-				m.NewConsumer(conf)
-			}
-		}
-		for _, v := range m.Consumers.All() {
+		for _, v := range m.consumers {
 			go v.Run()
 		}
 	})
+}
+
+func (m *Manager) Stop() {
+	m.ctxCancel()
 }
